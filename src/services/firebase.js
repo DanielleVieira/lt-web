ca727ac41dfaf86ff6a45088e783e7ba7cb5fbe9
daniellevieira <danielle.vieira@ccc.ufcg.edu.br>
2022-12-04 15:59:11 -0300

    [lt] layout da página de login

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 dotenv.config();
 
 const app = initializeApp(JSON.parse(process.env.FIREBASE_CONFIG));
+const auth = getAuth(app);
 
 
-export { app };
+export { auth };
